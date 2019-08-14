163ba3ab9526d7fe1ab13f7bb230a49458edb7c6
Altay Toktassyn <altosha211@gmail.com>
2019-08-14 20:53:48 +0600

    sql try

--- WebBlog/settings.py
+++ WebBlog/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATIC_ROOT = [
     os.path.join(BASE_DIR, 'assets')
 ]
 
