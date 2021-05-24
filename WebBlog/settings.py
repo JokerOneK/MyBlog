e5ca557fa4d4bb7553842f3698b4445ba3256e4d
Алтай <altosha211@gmail.com>
2021-05-24 22:30:31 +0600

    Updated static files for new html templates.

--- WebBlog/settings.py
+++ WebBlog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '#7467x@yxxb^=swq&lsdiu74qhx%n0
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = ['altayblog.herokuapp.com', '127.0.0.1', 'www.atblogster.com']
 
