34cdd4b7cbeb8d23f62e6f213297afd1e5e804b7
Алтай <altosha211@gmail.com>
2021-05-24 22:27:49 +0600

    Updated static files for new html templates.

--- WebBlog/settings.py
+++ WebBlog/settings.py
@@ -134,7 +134,8 @@ STATICFILES_DIRS = [
 
 
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
